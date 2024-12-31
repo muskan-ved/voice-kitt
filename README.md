81e5c3b7f2b986f906938da862f2f32d45055546
govindmango <govind@mangoitsolutions.com>
2024-12-31 17:49:37 +0530

    initi

---
+++ README.md
@@ -0,0 +1,10 @@
+# React + TypeScript + Vite
+
+# version
+node version : 19.4.0
+vite version : 4.4.5
+
+# installation process
+npm install 
+npm run dev
+
---
+++ src/Screens/WelcomeScreen/styles.ts
@@ -0,0 +1,66 @@
+export const Style = {
+  screenCardStyle: {
+    height: "100vh",
+    background: "linear-gradient(#040022, #011137)",
+    margin: "0 auto",
+  },
+
+  screenCardContentStyle: {
+    height: "100%",
+    position: "relative",
+  },
+
+  labelDivStyle: {
+    textAlign: "center !important",
+    margin: " 6% auto !important",
+    maxWidth: "500px !important",
+    maxHeight: "60% !important",
+    overflow: "auto !important",
+  },
+
+  labelWelcomeTextStyle: {
+    color: "#fff",
+    textAlign: "center",
+  },
+
+  labelTranscriptTextStyle: {
+    color: "#fff",
+    wordWrap: "break-word !important",
+    textAlign: "justify !important",
+    whiteSpace: "wrap",
+    padding: "5px",
+  },
+
+  labelTextStyle: {
+    color: "#9b9c9da3",
+    textAlign: "center",
+  },
+
+  cardActionStyle: {
+    position: "absolute",
+    bottom: "60px",
+    width: "100%",
+    left: 0,
+    margin: "0 auto",
+    justifyContent: "center",
+  },
+
+  buttonHelperTextColor: {
+    color: "#9b9c9d",
+  },
+
+  buttonStyle: {
+    border: "2px solid #4952f3 !important",
+    borderRadius: "50% !important",
+    height: "64px !important",
+    backgroundColor: "transparent !important",
+    marginTop: "10px !important",
+    ":hover": { border: "2px solid #4952f3 !important" },
+    ":focus": { outline: "none !important" },
+  },
+
+  avatarStyle: {
+    color: "#000",
+    background: "rgb(59 130 246)",
+  },
+};
